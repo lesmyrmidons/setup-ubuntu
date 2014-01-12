58df1b7ae750ea257a34923796e442d462d49523
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2014-01-12 11:52:02 +0100

    Update installation after testing

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Automation installation of Desktop
 Requirements
 -----------
 
-* Package curl
+* Package curl, git and git-core
 
 Run
 ---
 
 ```shell
-$ curl -L http://git.karbouin.net/kevin.arbouin/desktop-home/blob/master/install.sh | sudo sh
+$ curl -L http://git.karbouin.net/kevin.arbouin/desktop-home/blob/master/install.sh | sh
 ```
--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ CURRENT=`pwd`
 
 if [ ! -d ~/.config-desktop-home] then
     echo "\033[0;32m Installation de python.......\033[0m"
-    apt-get install -y -qq python git git-core
+    sudo apt-get install -y -qq python git git-core
 
     echo "\033[0;32m Installation d'ansible.......\033[0m"
-    pip install ansible
+    sudo pip install ansible
 
     echo "\033[0;32m Clone de la config d'installation.\033[0m"
     sudo -u `whoami` -H git clone git@git.karbouin.net:kevin.arbouin/desktop-home.git $HOME/.config-desktop-home
